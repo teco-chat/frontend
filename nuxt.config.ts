8ccd3222887d8bc7f013fd521a9dacd646fc9294
greeng00se <thegreengoose7@gmail.com>
2023-04-22 17:36:55 +0900

    feat: mdi 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
   build: {
     transpile: ['vuetify'],
   },
