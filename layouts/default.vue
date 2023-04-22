a03ccf032c4d23bfad950c7d0e8d8c85e5e96ab0
greeng00se <thegreengoose7@gmail.com>
2023-04-22 17:53:41 +0900

    feat: 레이아웃 설정 추가

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout>
+    <v-app-bar flat>
+      <v-app-bar-title class="title">우아GPT</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="toggleTheme" :icon="themeIcon"></v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-divider></v-divider>
+      <slot />
+    </v-main>
+  </v-layout>
+</template>
+
+<script lang="ts" setup>
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+const themeIcon = ref("mdi-weather-sunny");
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+  themeIcon.value =
+    themeIcon.value == "mdi-weather-night"
+      ? "mdi-weather-sunny"
+      : "mdi-weather-night";
+};
+
+</script>
+
+<style scoped>
+.title {
+    font-family: "Do Hyeon"
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-   <h1>Welcome to the homepage</h1>
+    <v-container>
+    <h1>안녕하세요.</h1>
+    <h1>우아한테크코스 크루들을 위한 Chat-GPT 서비스입니다.</h1>
+    </v-container>
   </div>
 </template>
+
+<style scoped>
+* {
+    font-family: "IBM Plex Sans KR", Arial, Verdana, Tahoma, sans-serif;
+}
+</style>
