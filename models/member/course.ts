c7b9275dff2f8f404bb502a8ae8fff94fb0bf9f0
greeng00se <thegreengoose7@gmail.com>
2023-04-27 21:42:30 +0900

    feat: 코스 클래스 추가

---
+++ models/member/course.ts
@@ -0,0 +1,14 @@
+export const COURSE = {
+  BE: {
+    key: "백엔드",
+    value: "backend",
+  },
+  FE: {
+    key: "프론트엔드",
+    value: "frontend",
+  },
+  AD: {
+    key: "안드로이드",
+    value: "android",
+  },
+} as const;
