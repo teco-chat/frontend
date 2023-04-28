4f3ad7c00014dd06dd9a1427d53f4178873d65ef
greeng00se <thegreengoose7@gmail.com>
2023-04-29 01:12:43 +0900

    feat: 단일 조회시 코드 블럭으로 출력하는 기능 추가

--- stores/item.ts
+++ stores/item.ts
@@ -15,12 +15,27 @@ export const useItemStore = defineStore("item", () => {
     }
     load.value = true;
     const { data, error } = await useFetch(
-      useRuntimeConfig().public.baseUrl + "/chats/" + id
+      useRuntimeConfig().public.baseUrl + "/chats/" + id, {
+        method: "GET",
+      }
     );
 
     const result: any = data;
     item.value = result.value;
+    for (let i = 0; i < item.value.messages.length; i++) {
+      item.value.messages[i].content = replaceCodeFences(item.value.messages[i].content);
+    }
     load.value = false;
+  };
+
+  const replaceCodeFences = (input: String) => {
+    const codeFencesRegex = /```([\w-]*)\n([\s\S]*?)\n```/g;
+    return input
+      .replace(codeFencesRegex, (match, p1, p2) => {
+        const languageClass = p1 ? ` class="language-${p1}"` : "";
+        return `<pre><code${languageClass}>${p2}</code></pre>`;
+      })
+      .replace(/\n/g, "<br>");
   };
 
   return { item, getItem };
