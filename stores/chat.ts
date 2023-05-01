ecf70e1c4a61d810739d28e2f2f8c75085885ebc
greeng00se <thegreengoose7@gmail.com>
2023-05-01 22:06:25 +0900

    [#8] fix: 제네릭을 사용할 때 괄호 내부가 안보이는 문제 수정

--- stores/chat.ts
+++ stores/chat.ts
@@ -79,6 +79,8 @@ export const useChatStore = defineStore("chat", () => {
   const replaceCodeFences = (input: String) => {
     const codeFencesRegex = /```([\w-]*)\n([\s\S]*?)\n```/g;
     return input
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
       .replace(codeFencesRegex, (match, p1, p2) => {
         const languageClass = p1 ? ` class="language-${p1}"` : "";
         return `<pre><code${languageClass}>${p2}</code></pre>`;
--- stores/item.ts
+++ stores/item.ts
@@ -29,12 +29,15 @@ export const useItemStore = defineStore("item", () => {
   const replaceCodeFences = (input: String) => {
     const codeFencesRegex = /```([\w-]*)\n([\s\S]*?)\n```/g;
     return input
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
       .replace(codeFencesRegex, (match, p1, p2) => {
         const languageClass = p1 ? ` class="language-${p1}"` : "";
         return `<pre><code${languageClass}>${p2}</code></pre>`;
       })
       .replace(/\n/g, "<br>");
   };
+  
 
   return { item, getItem };
 });
