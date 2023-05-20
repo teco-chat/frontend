67975f8752801d893af1637df2b68d8ab03e57b4
greeng00se <thegreengoose7@gmail.com>
2023-05-20 17:42:11 +0900

    [#40] feat: 단일 조회에서 좋아요 누른 사람 확인 기능 추가

--- stores/item.ts
+++ stores/item.ts
@@ -4,7 +4,18 @@ import { useAuthStore } from "./auth";
 
 export const useItemStore = defineStore("item", () => {
   const item = ref();
+  const likeCrew: any = ref([]);
   const load = ref(false);
+
+  const likeCrewName = () => {
+    if (likeCrew.value.length == 0) {
+      return '첫 좋아요를 눌러주세요!'
+    }
+    if (likeCrew.value.length >= 10) {
+      return likeCrew.value.slice(0, 10).join("<br />") + "<br />..."
+    }
+    return likeCrew.value.join("<br />");
+  }
 
   const searchById = async (id: string) => {
     if (load.value) {
@@ -16,7 +27,7 @@ export const useItemStore = defineStore("item", () => {
       {
         headers: {
           name: useAuthStore().encodedName(),
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         method: "GET",
       }
@@ -32,15 +43,35 @@ export const useItemStore = defineStore("item", () => {
     load.value = false;
   };
 
+  const searchLikeCrewById = async (id: string) => {
+    if (load.value) {
+      return;
+    }
+    load.value = true;
+    const { data, error } = await useFetch(
+      useRuntimeConfig().public.baseUrl + "/chat-likes?chatId=" + id
+    );
+    likeCrew.value = [];
+
+    const result: any = data;
+    for (let i = 0; i < result.value.length; i++) {
+      likeCrew.value.push(result.value[i].memberInfo.crewName);
+    }
+    load.value = false;
+  };
+
   const like = async () => {
     if (item.value["isAlreadyClickLike"]) {
       item.value["likeCount"] -= 1;
       item.value["isAlreadyClickLike"] = false;
+      let name: string = useAuthStore().name;
+      likeCrew.value = likeCrew.value.filter((crewName: string) => crewName != name);
     } else {
       item.value["likeCount"] += 1;
       item.value["isAlreadyClickLike"] = true;
+      likeCrew.value.push(useAuthStore().name);
     }
-  }
+  };
 
-  return { item, like, searchById };
+  return { item, likeCrewName, like, searchById, searchLikeCrewById };
 });
