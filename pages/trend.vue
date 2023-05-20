c55b2bdfe35403a87dadf4a2018c0fad90439086
greeng00se <thegreengoose7@gmail.com>
2023-05-20 15:38:43 +0900

    [#44] feat: 좋아요 순서로 정렬 기능 추가

---
+++ pages/trend.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <v-container>
+      <div class="pa-4">
+        <v-chip-group
+          selected-class="text-secondary"
+          column
+          v-model="trendStore.trendIndex"
+          mandatory
+        >
+          <v-chip
+            v-for="tag in TREND"
+            :key="tag"
+            filter
+            @click="clearAndSearch"
+          >
+            {{ tag.key }}
+          </v-chip>
+        </v-chip-group>
+      </div>
+      <v-row>
+        <v-col
+          v-for="chat in trendStore.items"
+          cols="12"
+          sm="4"
+          lg="4"
+          xl="3"
+          xxl="2"
+          :key="chat.title"
+        >
+          <v-card :to="'/chat/' + chat.id" align="left" min-height="240px">
+            <div class="d-flex mr-4 align-center">
+              <v-chip
+                class="ma-2"
+                color="secondary"
+                text-color="white"
+                prepend-icon="mdi-account-circle"
+              >
+                {{ chat.crewName }}
+              </v-chip>
+              <v-chip
+                v-if="chat.course == 'BACKEND'"
+                class="ma-2"
+                color="success"
+                text-color="white"
+                prepend-icon="mdi-language-java"
+              >
+                백엔드
+              </v-chip>
+              <v-chip
+                v-if="chat.course == 'FRONTEND'"
+                class="ma-2"
+                color="info"
+                text-color="white"
+                prepend-icon="mdi-language-typescript"
+              >
+                프론트엔드
+              </v-chip>
+              <v-chip
+                v-if="chat.course == 'ANDROID'"
+                class="ma-2"
+                color="primary"
+                text-color="white"
+                prepend-icon="mdi-language-kotlin"
+              >
+                안드로이드
+              </v-chip>
+              <v-spacer></v-spacer>
+            </div>
+            <v-card-item>
+              <v-card-title>{{ chat.title }}</v-card-title>
+              <v-card-subtitle>{{
+                parseDateTimeFormat(chat.createdAt)
+              }}</v-card-subtitle>
+            </v-card-item>
+            <chipdiv v-for="keyword in chat.keywords" :key="keyword">
+              <v-chip size="small" class="ma-2" color="warning" label>
+                {{ '#' + keywordShortener(keyword.keyword) }}
+              </v-chip>
+            </chipdiv>
+            <v-card-actions class="card-actions">
+              <v-list-item class="w-100">
+                <template v-slot:append>
+                  <div class="justify-self-end">
+                    <v-icon
+                      class="me-2"
+                      icon="mdi-comment-question-outline"
+                    ></v-icon>
+                    <span class="subheading me-4">{{
+                      chat.totalQnaCount
+                    }}</span>
+                    <v-icon
+                      class="me-2"
+                      color="error"
+                      icon="mdi-heart-outline"
+                    ></v-icon>
+                    <span class="subheading">{{ chat.likeCount }}</span>
+                  </div>
+                </template>
+              </v-list-item>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-card v-intersect="searchNext"></v-card>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useTrendStore } from "~/stores/trend";
+import { TREND } from "~/models/trend/trend";
+import { parseDateTimeFormat } from "~~/utils/date";
+import Tiptap from "~/components/Tiptap.vue";
+
+const isIntersect = ref(false);
+const trendStore = useTrendStore();
+
+const searchNext = async () => {
+  if (isIntersect.value) {
+    return;
+  }
+  isIntersect.value = true;
+  await trendStore.searchNext();
+  isIntersect.value = false;
+};
+
+const clearAndSearch = async () => {
+  await trendStore.clear();
+  await trendStore.searchNext();
+};
+
+const keywordShortener = (keyword: string) => {
+  return keyword.substring(0, 20) + (keyword.length > 20 ? "..." : "");
+}
+
+clearAndSearch();
+</script>
+
+<style scoped>
+.card-actions {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+</style>
