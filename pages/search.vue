8334b28f73dd88d4d9049526cea89f1d15ae6e10
greeng00se <thegreengoose7@gmail.com>
2023-06-09 22:17:13 +0900

    [#41] feat: 레이아웃에 검색 아이콘 추가

---
+++ pages/search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-container align="center">
+      <v-card align="left" max-width="800px">
+        Hello World
+      </v-card>
+    </v-container>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useAuthStore } from "~~/stores/auth";
+
+</script>
+
+<style scoped>
+* {
+  font-family: "IBM Plex Sans KR", Arial, Verdana, Tahoma, sans-serif;
+}
+</style>
